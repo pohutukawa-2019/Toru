52a9955e15c24ca8802704331d9cd5dc10068a02
Katrina Turner <katrinaturner333@gmail.com>
2019-09-13 11:14:02 +1200

    edited component files only

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,1 +1,11 @@
-// "Truth Or Dare Or Deer"
+import React from 'react'
+
+function Header () {
+  return (
+    <div className='header'>
+      <h1 className='brand-title'>PLAY Truth or Dare or Deer</h1>
+    </div>
+  )
+}
+
+export default Header
