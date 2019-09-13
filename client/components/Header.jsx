1ddf3428b7a5c61519e01f4956b2febbec693da0
Henry Peti <iamgadgetboy@gmail.com>
2019-09-13 14:53:18 +1200

    yo

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import {Motion, spring} from 'react-motion';
 
 function Header () {
   return (
     <div className='header'>
       <h1 className='brand-title'>PLAY Truth or Dare or Deer</h1>
+      // In your render...
+      <Motion headerStyle={{x: 0}} style={{x: spring(10)}}>
+        {value => <div>{value.x}</div>}
+      </Motion>
     </div>
   )
 }
+
+
+
 
 export default Header
